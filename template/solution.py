00a679df164893b6bea72d4e2eaadf69ebca1fe9
Ryan Zhu <36178918+alfphaderp@users.noreply.github.com>
2020-12-08 21:31:38 -0800

    Add commented integer conversion for convenience

--- template/solution.py
+++ template/solution.py
@@ -7,5 +7,6 @@ def part2(lines):
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         lines = f.read().splitlines()
+    # lines = [int(i) for i in lines]
     print(part1(lines))
     print(part2(lines))
