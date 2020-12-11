a7089c2419d55adce179d57d2a1af210e2a9c42e
Ryan Zhu <36178918+alfphaderp@users.noreply.github.com>
2020-12-11 13:50:36 -0800

    Clean day 11 optimized

--- day11/solution_optimized.py
+++ day11/solution_optimized.py
@@ -12,16 +12,8 @@ def is_valid(i, j):
 def count_occupied(seats, coords):
     return [seats[i][j] for i, j in coords].count('#')
 
-def part1(seats):
-    def find_neighbors(r, c):
-        neighbors = []
-        for dr, dc in NEIGHBORS:
-            i, j = r + dr, c + dc
-            if is_valid(i, j) and seats[i][j] != '.':
-                neighbors.append((i, j))
-        return neighbors
-    
-    neighbors = [[find_neighbors(i, j) for j in range(COLS)] for i in range(ROWS)]
+def simulate(seats, neighbor_func, tolerance):
+    neighbors = [[neighbor_func(i, j) for j in range(COLS)] for i in range(ROWS)]
     
     def update(seats, coords):
         unstable_coords = set()
@@ -31,7 +23,7 @@ def part1(seats):
             if seats[i][j] == 'L' and occupied == 0:
                 updates.append((i, j, '#'))
                 unstable_coords.update(neighbors[i][j])
-            elif seats[i][j] == '#' and occupied >= 4:
+            elif seats[i][j] == '#' and occupied >= tolerance:
                 updates.append((i, j, 'L'))
                 unstable_coords.update(neighbors[i][j])
         for i, j, val in updates:
@@ -44,6 +36,17 @@ def part1(seats):
         coords = update(seats, coords)
     
     return sum(r.count('#') for r in seats)
+
+def part1(seats):
+    def find_neighbors(r, c):
+        neighbors = []
+        for dr, dc in NEIGHBORS:
+            i, j = r + dr, c + dc
+            if is_valid(i, j) and seats[i][j] != '.':
+                neighbors.append((i, j))
+        return neighbors
+    
+    return simulate(seats, find_neighbors, 4)
 
 def part2(seats):
     def find_los_neighbors(r, c):
@@ -56,29 +59,7 @@ def part2(seats):
                 neighbors.append((i, j))
         return neighbors
     
-    neighbors = [[find_los_neighbors(i, j) for j in range(COLS)] for i in range(ROWS)]
-
-    def update(seats, coords):
-        unstable_coords = set()
-        updates = []
-        for i, j in coords:
-            occupied = count_occupied(seats, neighbors[i][j])
-            if seats[i][j] == 'L' and occupied == 0:
-                updates.append((i, j, '#'))
-                unstable_coords.update(neighbors[i][j])
-            elif seats[i][j] == '#' and occupied >= 5:
-                updates.append((i, j, 'L'))
-                unstable_coords.update(neighbors[i][j])
-        for i, j, val in updates:
-            seats[i][j] = val
-        return unstable_coords
-    
-    coords = [(i, j) for i in range(ROWS) for j in range(COLS)]
-    seats = [list(r) for r in seats]
-    while coords:
-        coords = update(seats, coords)
-    
-    return sum(r.count('#') for r in seats)
+    return simulate(seats, find_los_neighbors, 5)
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
